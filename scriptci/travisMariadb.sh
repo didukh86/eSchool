427b57dad2164aa43989fed894999f3285c64e3f
Taras Didukh <didukh86@gmail.com>
2021-03-19 11:56:03 +0200

    Mariadb added2

--- scriptci/travisMariadb.sh
+++ scriptci/travisMariadb.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo debconf-set-selections <<< 'mariadb-server-10.3 mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mariadb-server-10.3 mysql-server/root_password_again password root'
-sudo apt install mariadb-server-10.3 -y
-echo 'Finnished reinstalling MariaDB Server 10.3'
-
-
 # create database and user
 sudo mysql -e "CREATE DATABASE eschool DEFAULT CHARSET utf8 COLLATE utf8_unicode_ci;"
 sudo mysql -e "CREATE USER '$DATASOURCE_USERNAME'@'%' IDENTIFIED BY '$DATASOURCE_PASSWORD';"
