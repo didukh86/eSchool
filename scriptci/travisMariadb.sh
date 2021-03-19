9d9b3303266051064d42e3f8fd56dd5fde3c95f2
Taras Didukh <didukh86@gmail.com>
2021-03-19 12:00:18 +0200

    Mariadb added3

--- scriptci/travisMariadb.sh
+++ scriptci/travisMariadb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # create database and user
-sudo mysql -e "CREATE DATABASE eschool DEFAULT CHARSET utf8 COLLATE utf8_unicode_ci;"
-sudo mysql -e "CREATE USER '$DATASOURCE_USERNAME'@'%' IDENTIFIED BY '$DATASOURCE_PASSWORD';"
-sudo mysql -e "GRANT ALL ON eschool.* TO $DATASOURCE_USERNAME@'%';"
+mysql -e "CREATE DATABASE eschool DEFAULT CHARSET utf8 COLLATE utf8_unicode_ci;"
+mysql -e "CREATE USER '$DATASOURCE_USERNAME'@'%' IDENTIFIED BY '$DATASOURCE_PASSWORD';"
+mysql -e "GRANT ALL ON eschool.* TO $DATASOURCE_USERNAME@'%';"
 
 echo 'USER created'
