1d62b84102ee774f279ec273af7312f824bfd1fc
Taras Didukh <didukh86@gmail.com>
2021-03-22 12:29:25 +0200

    Deployment scripts added11

--- scriptci/updateRun.sh
+++ scriptci/updateRun.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-apt install openssh-server
-echo "SSH server installed"
 
-chmod 600 ./FirstKeyPair.pem
 ssh -i ./FirstKeyPair.pem ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
 
