06166f5312dc2c8f8ff884806d2ea298c76a6861
Taras Didukh <didukh86@gmail.com>
2021-03-22 15:54:20 +0200

    Deploy added7

--- scriptci/update.sh
+++ scriptci/update.sh
@@ -3,7 +3,7 @@ date
 sudo apt install net-tools
 cd /home/ubuntu
 
-sudo systemctl stop eschool
+sudo killall -9 java
 wget https://eschoolbucket.s3.us-east-2.amazonaws.com/eschool.jar
 sudo java -jar -eschool.jar
 exit
