877e3f151ee8a70a4bd866547f2ef7c492caf217
Taras Didukh <didukh86@gmail.com>
2021-03-22 16:14:59 +0200

    Deploy added9

--- scriptci/updateRun.sh
+++ scriptci/updateRun.sh
@@ -4,7 +4,8 @@ if sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsF
 then
 echo "succefully passed update 1"
  sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
-echo "succefully passed update 1"
+else
+ sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
 fi
 
 3.142.198.62
