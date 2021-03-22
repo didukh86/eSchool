32a1c79bea4b05f581cbdbc502175d0a248bd11e
Taras Didukh <didukh86@gmail.com>
2021-03-22 15:28:26 +0200

    Deploy added3

--- scriptci/update.sh
+++ scriptci/update.sh
@@ -4,4 +4,5 @@ sudo apt install net-tools
 cd /home/ubuntu
 
 wget https://eschoolbucket.s3.us-east-2.amazonaws.com/eschool.jar
-exit
+return
+
--- scriptci/updateRun.sh
+++ scriptci/updateRun.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-
-sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
-
+if sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
+then
+echo "succefully passed update 1"
+ sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@3.142.198.62 'bash -s' < ./scriptci/update.sh
+echo "succefully passed update 1"
+fi
