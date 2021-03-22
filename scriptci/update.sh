93be3a601de1e4192958a31d612e860bceb69455
Taras Didukh <didukh86@gmail.com>
2021-03-22 15:49:55 +0200

    Deploy added6

--- scriptci/update.sh
+++ scriptci/update.sh
@@ -3,7 +3,8 @@ date
 sudo apt install net-tools
 cd /home/ubuntu
 
+sudo systemctl stop eschool
 wget https://eschoolbucket.s3.us-east-2.amazonaws.com/eschool.jar
-sudo java.jar -eschool.jar
+sudo java -jar -eschool.jar
 exit
 
--- scriptci/updateRun.sh
+++ scriptci/updateRun.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
+if sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@3.142.198.62 'bash -s' < ./scriptci/update.sh
 then
 echo "succefully passed update 1"
- sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@3.142.198.62 'bash -s' < ./scriptci/update.sh
+ sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
 echo "succefully passed update 1"
 fi
+
+3.142.198.62
