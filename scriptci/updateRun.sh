523f280be5450a3af2c0e76c6d5b952ad2caa4ed
Taras Didukh <didkh86@gmail.com>
2021-03-23 18:17:08 +0200

    eschool deploy added

--- scriptci/updateRun.sh
+++ scriptci/updateRun.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@3.142.198.62 'bash -s' < ./scriptci/update.sh
+if sudo ssh -i ./eschool-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@3.142.198.62 'bash -s' < ./scriptci/update.sh
 then
 echo "succefully passed update 1"
- sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
+ sudo ssh -i ./eschool-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
 else
- sudo ssh -i ./FirstKeyPair.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
+ sudo ssh -i ./eschool-key.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@18.189.184.188 'bash -s' < ./scriptci/update.sh
 fi
 
-3.142.198.62
+
