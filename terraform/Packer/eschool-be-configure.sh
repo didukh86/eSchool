44b3731b21e2c51c86d033343eb56aa86b9eed0b
Taras Didukh <didkh86@gmail.com>
2021-04-03 20:38:28 +0300

    Packer filles added

---
+++ terraform/Packer/eschool-be-configure.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install -y mariadb-client-core-10.3
+sudo apt-get install -y openjdk-8-jdk
+cd /home/ubuntu
+wget -O eschool.jar https://eschoolbucket.s3.us-east-2.amazonaws.com/eschool.jar
+sudo chmod +x eschool.jar
+
+sudo echo "[Service]
+ExecStart=/home/ubuntu/eschoolstart.sh
+Restart=always
+WorkingDirectory=/home/ubuntu
+User=root
+Restart=on-failure
+RestartSec=30
+
+SuccessExitStatus=143
+TimeoutStopSec=10
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/eschool.service
+
+sudo echo "#!/bin/bash
+
+sudo java -jar /home/ubuntu/eschool.jar" > /home/ubuntu/eschoolstart.sh
+
+sudo chmod +x /home/ubuntu/eschoolstart.sh
+
+sudo systemctl daemon-reload
+sudo systemctl enable eschool
+sudo systemctl start eschool
---
+++ terraform/Packer/eschoolstart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo java -jar /home/ubuntu/eschool.jar
